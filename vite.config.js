d2105fadb46a291b7633d606d7f031673b5f0d9f
MicroThunder <nurdin.reverse73@gmail.com>
2024-10-26 23:04:58 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,35 @@ import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { join, resolve } from 'path'
 
-export default ({ mode }) => {
+export default defineConfig(({ command, mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-
-  return defineConfig({
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      define: {
+        global: {},
+      },
+      resolve: {
+        alias: [
+          {
+            find: /~(.+)/,
+            replacement: join(process.cwd(), 'node_modules/$1'),
+          },
+          { find: '@', replacement: resolve(__dirname, './resources/ts') },
+        ],
+      },
+      server: {
+        port: process.env.VITE_PORT || 5137,
+      },
+    }
+  }
+  return {
     plugins: [
       react(),
-      //   laravel({
-      //     input: ['resources/ts/index.tsx'],
-      //     refresh: true,
-      //   }),
+      laravel({
+        input: ['resources/ts/index.tsx'],
+        refresh: true,
+      }),
     ],
     define: {
       global: {},
@@ -29,5 +48,5 @@ export default ({ mode }) => {
     server: {
       port: process.env.VITE_PORT || 5137,
     },
-  })
-}
+  }
+})
