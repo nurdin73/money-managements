7b596a54bb98bd046a2041f88ab9efdfe994bf56
MicroThunder <nurdin.reverse73@gmail.com>
2024-12-27 00:32:22 +0700

    feat: remove unused notification icon from AppHeader component

--- resources/ts/components/AppHeader.tsx
+++ resources/ts/components/AppHeader.tsx
@@ -52,13 +52,6 @@ const AppHeader = () => {
           <CIcon icon={cilMenu} size='lg' />
         </CHeaderToggler>
         <CHeaderNav className='ms-auto'>
-          <CNavItem>
-            <CNavLink href='#'>
-              <CIcon icon={cilBell} size='lg' />
-            </CNavLink>
-          </CNavItem>
-        </CHeaderNav>
-        <CHeaderNav>
           <li className='nav-item py-1'>
             <div className='vr h-100 mx-2 text-body text-opacity-75'></div>
           </li>
